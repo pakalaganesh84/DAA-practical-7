a1f0b9e2a937f83f6b2fc4b2c0834cd4f0ae9a98
pakalaganesh84 <pakalaganesh84@gmail.com>
2026-09-24 13:53:02 +0530

    Implement coin change problem solution in C++

---
+++ coin change problem.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n, amount;
+
+    cout << "Enter number of coins: ";
+    cin >> n;
+
+    int coin[n];
+
+    cout << "Enter coin values: ";
+    for (int i = 0; i < n; i++) {
+        cin >> coin[i];
+    }
+
+    cout << "Enter amount: ";
+    cin >> amount;
+
+    int dp[amount + 1];
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; i++) {
+        dp[i] = INT_MAX;
+    }
+
+    for (int i = 1; i <= amount; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (coin[j] <= i && dp[i - coin[j]] != INT_MAX) {
+                dp[i] = min(dp[i],
+                            dp[i - coin[j]] + 1);
+            }
+        }
+    }
+
+    if (dp[amount] == INT_MAX) {
+        cout << "Amount cannot be made using given coins.";
+    }
+    else {
+        cout << "Minimum number of coins = "
+             << dp[amount];
+    }
+
+    return 0;
+}
